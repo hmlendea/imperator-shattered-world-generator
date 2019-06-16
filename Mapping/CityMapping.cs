cf807875d4fd799d3e6f2e9b40c956e7203012a0
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-06-16 16:53:59 +0300

    Better file handling + pop generation

--- Mapping/CityMapping.cs
+++ Mapping/CityMapping.cs
@@ -14,10 +14,38 @@ namespace ImperatorShatteredWorldGenerator.Mapping
             serviceModel.Id = dataObject.Id;
             serviceModel.CultureId = dataObject.CultureId;
             serviceModel.ReligionId = dataObject.ReligionId;
+            serviceModel.TradeGoodId = dataObject.TradeGoodId;
+            serviceModel.TribesmenCount = dataObject.TribesmenCount;
+            serviceModel.CitizensCount = dataObject.CitizensCount;
+            serviceModel.FreemenCount = dataObject.FreemenCount;
+            serviceModel.TribesmenCount = dataObject.TribesmenCount;
+            serviceModel.SlavesCount = dataObject.SlavesCount;
+            serviceModel.CivilizationLevel = dataObject.CivilizationLevel;
+            serviceModel.BarbarianLevel = dataObject.BarbarianLevel;
             serviceModel.NameId = dataObject.NameId;
             serviceModel.ProvinceId = dataObject.ProvinceId;
 
             return serviceModel;
+        }
+
+        internal static CityEntity ToDataObject(this City serviceModel)
+        {
+            CityEntity dataObject = new CityEntity();
+            dataObject.Id = serviceModel.Id;
+            dataObject.CultureId = serviceModel.CultureId;
+            dataObject.ReligionId = serviceModel.ReligionId;
+            dataObject.TradeGoodId = serviceModel.TradeGoodId;
+            dataObject.TribesmenCount = serviceModel.TribesmenCount;
+            dataObject.CitizensCount = serviceModel.CitizensCount;
+            dataObject.FreemenCount = serviceModel.FreemenCount;
+            dataObject.TribesmenCount = serviceModel.TribesmenCount;
+            dataObject.SlavesCount = serviceModel.SlavesCount;
+            dataObject.CivilizationLevel = serviceModel.CivilizationLevel;
+            dataObject.BarbarianLevel = serviceModel.BarbarianLevel;
+            dataObject.NameId = serviceModel.NameId;
+            dataObject.ProvinceId = serviceModel.ProvinceId;
+
+            return dataObject;
         }
 
         internal static IEnumerable<City> ToServiceModels(this IEnumerable<CityEntity> dataObjects)
