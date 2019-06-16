fdce9ba0e7d88f78d5bf257175a900ffd7c933ae
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-06-16 17:29:47 +0300

    Random religions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
+﻿using NuciCLI;
 
-using NuciCLI;
-using NuciDAL.Repositories;
-
-using ImperatorShatteredWorldGenerator.DataAccess.DataObjects;
+using ImperatorShatteredWorldGenerator.Service;
 
 namespace ImperatorShatteredWorldGenerator
 {
@@ -17,14 +11,12 @@ namespace ImperatorShatteredWorldGenerator
 
         static void Main(string[] args)
         {
-            string imperatorDirectoryPath = CliArgumentsReader.GetOptionValue(args, ImperatorDirectoryPathOptions);
+            string gameDirectory = CliArgumentsReader.GetOptionValue(args, ImperatorDirectoryPathOptions);
             string outputModPath = CliArgumentsReader.GetOptionValue(args, OutputModPathOptions);
 
-            string vanillaProvinceSetupFilePath = Path.Combine(imperatorDirectoryPath, "game", "common", "province_setup.csv");
+            IEntitiesLoader entitiesLoader = new EntitiesLoader(gameDirectory);
 
-            CsvRepository<CityEntity> vanillaCityRepository = new CsvRepository<CityEntity>(vanillaProvinceSetupFilePath);
-
-            Generator generator = new Generator(vanillaCityRepository);
+            Generator generator = new Generator(entitiesLoader, outputModPath);
 
             generator.Generate();
             generator.Save(outputModPath);
