6f961d48c66ba9ddfacd375099952f3e962b7311
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-06-16 19:43:11 +0300

    Countries generation

---
+++ Service/Models/Country.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace ImperatorShatteredWorldGenerator.Service.Models
+{
+    public sealed class Country
+    {
+        public string Id { get; set; }
+
+        public int ColourRed { get; set; }
+
+        public int ColourGreen { get; set; }
+
+        public int ColourBlue { get; set; }
+
+        public string CapitalId { get; set; }
+
+        public string CultureId { get; set; }
+
+        public string ReligionId { get; set; }
+
+        public IEnumerable<string> ShipNames { get; set; }
+
+        public Country()
+        {
+            ShipNames = new List<string> { "Boaty", "McBoatface", "Boatface", "Shippy", "McShipface", "Shipface" };
+        }
+    }
+}
