b946a022fcb9f1e0a5a6257f8e4191d9a8d8507f
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-06-17 19:46:02 +0300

    RNG seed input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using NuciCLI;
+﻿using System;
+
+using NuciCLI;
 
 using ImperatorShatteredWorldGenerator.Service;
 
@@ -8,15 +10,26 @@ namespace ImperatorShatteredWorldGenerator
     {
         static string[] ImperatorDirectoryPathOptions = new string[] { "-d", "--dir", "--game", "--imperator" };
         static string[] OutputModPathOptions = new string[] { "-o", "--out", "--output" };
+        static string[] SeedOptions = new string[] { "-s", "--seed" };
 
         static void Main(string[] args)
         {
             string gameDirectory = CliArgumentsReader.GetOptionValue(args, ImperatorDirectoryPathOptions);
             string outputModPath = CliArgumentsReader.GetOptionValue(args, OutputModPathOptions);
+            int seed;
+
+            if (CliArgumentsReader.HasOption(args, SeedOptions))
+            {
+                seed = int.Parse(CliArgumentsReader.GetOptionValue(args, SeedOptions));
+            }
+            else
+            {
+                seed = new Random().Next();
+            }
 
             IEntitiesLoader entitiesLoader = new EntitiesLoader(gameDirectory);
 
-            Generator generator = new Generator(entitiesLoader);
+            Generator generator = new Generator(entitiesLoader, seed);
 
             generator.Generate();
             generator.Save(outputModPath);
